b74ed460cac8a83ebab73be5d43df84b462e789a
Jordy Pena <jordypenamontes@gmail.com>
2021-05-10 14:36:58 -0500

    Footer.js component created and styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Home from './Home/Home';
 import Nav from './Nav/Nav';
-
+import Footer from './Footer/Footer';
 function App() {
   return (
     <>
     <Nav />
     <Home/>
+    <Footer />
     </>
   );
 }
