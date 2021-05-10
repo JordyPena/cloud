1f149d8e2b5f2e68f9244ddc3b4e415ddb3454ed
Jordy Pena <jordypenamontes@gmail.com>
2021-05-10 16:37:46 -0500

    Nav.js refactored navbar, new styles added

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,21 +1,22 @@
 import '../Nav/Nav.css';
 import { AiFillCloud } from 'react-icons/ai';
 const Nav = () => {
-
   return (
-    <navbar className='nav-container'>
-      <div className='nav-content-left'>
-        <p>About us</p>
-        <p>Shop</p>
+    <navbar className='nav-wrapper'>
+      <div className='nav-container-left'>
+        <h1 className='nav-header'>
+          Cloud <AiFillCloud />
+        </h1>
+        <span className='cloud-img'></span>
       </div>
-      <h1 className='nav-header'>Cloud <AiFillCloud/></h1>
-      <span className="cloud-img"></span>
-      <div className='nav-content-right'>
-        <p>Search</p>
-        <p>Cart</p>
+
+      <div className='nav-container-right'>
+        <p className='nav-content-right'>Search</p>   
+        <p className='nav-content-right'>Cart</p>   
+        <p className='nav-content-right'>About Us</p>   
       </div>
     </navbar>
-  )
-}
+  );
+};
 
 export default Nav;
