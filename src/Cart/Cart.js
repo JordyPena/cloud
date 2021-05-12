cbccd57dbba53595001962178a037d24e7f4dd22
Jordy Pena <jordypenamontes@gmail.com>
2021-05-11 23:07:10 -0500

    cart.js refactored html and styles

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -4,43 +4,60 @@ const Cart = () => {
   return (
     <>
       <main className="cart-wrapper">
-      <header className='cart-header'>
-        <p>It's time to review your cart.</p>
-      </header>
+        <header className="cart-header">
+          <p>Bag</p>
+        </header>
         <div className="cart-titles">
-          <p>Quantity</p>
-          <p>Product Name</p>
-          <p>Charge</p>
+          <p className='cart-titles-text'>3 Items</p>
+          <p className='cart-titles-text'>$270.00</p>
         </div>
         <div className="cart-items">
-          <p>1 item</p>
-          <p>Dope Shoes</p>
-          <p>$100.00</p>
+          <div className="cart-img-container"></div>
+          <div className="items-top">
+            <p>Nike Air Force 1 '07</p>
+            <p>$90.00</p>
+          </div>
         </div>
+        <div className="items-bottom">
+          <p>Men's Shoe</p>
+          <p>Size</p>
+          <p>M 12 / W 13.5</p>
+        </div>
+        <hr />
         <div className="cart-items">
-          <p>1 item</p>
-          <p>Dope Shoes</p>
-          <p>$100.00</p>
+          <div className="cart-img-container"></div>
+          <div className="items-top">
+            <p>Nike Air Force 1 '07</p>
+            <p>$90.00</p>
+          </div>
         </div>
+        <div className="items-bottom">
+          <p>Men's Shoe</p>
+          <p>Size</p>
+          <p>M 12 / W 13.5</p>
+        </div>
+        <hr />
         <div className="cart-items">
-          <p>1 item</p>
-          <p>Dope Shoes</p>
-          <p>$100.00</p>
+          <div className="cart-img-container"></div>
+          <div className="items-top">
+            <p>Nike Air Force 1 '07</p>
+            <p>$90.00</p>
+          </div>
         </div>
-        <div className="cart-items">
-          <p>1 item</p>
-          <p>Dope Shoes</p>
-          <p>$100.00</p>
+        <div className="items-bottom">
+          <p>Men's Shoe</p>
+          <p>Size</p>
+          <p>M 12 / W 13.5</p>
         </div>
-        <div className="cart-items">
-          <p>1 item</p>
-          <p>Dope Shoes</p>
-          <p>$100.00</p>
-        </div>
+        <hr />
         <section className="cart-total">
-          <p>Subtotal: $500.00</p>
-          <p>Estimated Tax: $41.25</p>
-          <p>Total: $541.25</p>
+          <p className="summary-title">Summary</p>
+          <div className="cart-total-text">
+            <p>Subtotal: $270.00</p>
+            <p>Estimated Shipping: $8.00</p>
+            <p>Estimated Tax: $22.93</p>
+            <p>Total: $292.93</p>
+          </div>
         </section>
         <section className="cart-button-container">
           <button className="shop-button-style">Shop More</button>
