814a2cc7542d8d9021f0361cdc1805de5a0fddaa
Jordy Pena <jordypenamontes@gmail.com>
2021-05-18 20:21:28 -0500

    nav.test.js -created test for all links using react router

--- src/Nav/Nav.test.js
+++ src/Nav/Nav.test.js
@@ -1,28 +1,57 @@
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import '@testing-library/jest-dom/extend-expect';
-import App from '../App';
+import { createMemoryHistory } from "history";
+import { Router } from "react-router";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom/extend-expect";
+import App from "../App";
 
-
-describe('Nav', () => {
-
-  test('directs to home page when cloud text clicked', () => {
+describe("Nav", () => {
+  test("directs to cart when cart text clicked", () => {
     const history = createMemoryHistory();
-    render (
+    render(
       <Router history={history}>
         <App />
       </Router>
-    )
-    expect(screen.getByText(/Keep the Summer Vibe Alive/i)).toBeInTheDocument()
-  
-    const leftClick = { button: 0}
-    userEvent.click(screen.getByText(/cart/i), leftClick)
+    );
+    expect(screen.getByText(/Keep the Summer Vibe Alive/i)).toBeInTheDocument();
+
+    const leftClick = { button: 0 };
+    userEvent.click(screen.getByText(/cart/i), leftClick);
 
     // check that the content changed to the new page
-  expect(screen.getByText(/bag/i)).toBeInTheDocument()
-  })
+    expect(screen.getByText(/bag/i)).toBeInTheDocument();
+  });
 
+  test("directs to about page when About Us text clicked", () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+    expect(screen.getByText(/Keep the Summer Vibe Alive/i)).toBeInTheDocument();
 
-})
+    const leftClick = { button: 0 };
+    userEvent.click(screen.getByText(/About Us/i), leftClick);
+
+    // check that the content changed to the new page
+    expect(screen.getByText(/let it drip/i)).toBeInTheDocument();
+  });
+
+  test("directs to home page when Cloud text clicked", () => {
+    const history = createMemoryHistory();
+    history.push("/about");
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+    expect(screen.getByText(/Let it drip/i)).toBeInTheDocument();
+
+    const leftClick = { button: 0 };
+    userEvent.click(screen.getByTestId("cloud-element"), leftClick);
+
+    // check that the content changed to the new page
+    expect(screen.getByText(/keep the summer vibe alive/i)).toBeInTheDocument();
+  });
+});
