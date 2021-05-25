2e5b2bb61a8cee9a29ab0b76f88ec791363e82bc
Jordy Pena <jordypenamontes@gmail.com>
2021-05-24 21:48:11 -0500

    nav.js refactored searchbar from component to prop from app

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,8 +1,8 @@
 import '../Nav/Nav.css';
 import { Link } from 'react-router-dom';
 import { AiFillCloud } from 'react-icons/ai';
-import Searchbar from '../Searchbar/Searchbar';
-const Nav = () => {
+
+const Nav = ({ allResults, searchbar }) => {
   return (
     <nav className='nav-wrapper'>
       <div className='nav-container-left'>
@@ -12,7 +12,7 @@ const Nav = () => {
       </div>
 
       <div className='nav-container-right'>
-        <Searchbar/>
+        {searchbar}
         <Link to='/cart' className='nav-content-right'>Cart</Link>   
         <Link to='about' className='nav-content-right'>About Us</Link>   
       </div>
