f6949c1a787e7d2a6e0b8b0c9ce61714213af4ab
Jordy Pena <jordypenamontes@gmail.com>
2021-05-29 11:07:32 -0500

    created test and also added a filter method for item search

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,20 +1,46 @@
-import '../Nav/Nav.css';
-import { Link } from 'react-router-dom';
-import { AiFillCloud } from 'react-icons/ai';
+import "../Nav/Nav.css";
+import { Link } from "react-router-dom";
+import { AiFillCloud } from "react-icons/ai";
+import { useState } from 'react';
+const Nav = ({ searchSubmitted }) => {
 
-const Nav = ({ allResults, searchbar }) => {
+  const [userInput, setUserInput] = useState("");
+
+  const inputChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const searchSubmit = (e) => {
+    e.preventDefault();
+    searchSubmitted(userInput)
+  }
+
   return (
-    <nav className='nav-wrapper'>
-      <div className='nav-container-left'>
-        <Link to='/' className='nav-header' data-testid='cloud-element'>
+    <nav className="nav-wrapper">
+      <div className="nav-container-left">
+        <Link to="/" className="nav-header" data-testid="cloud-element">
           Cloud <AiFillCloud />
         </Link>
       </div>
 
-      <div className='nav-container-right'>
-        {searchbar}
-        <Link to='/cart' className='nav-content-right'>Cart</Link>   
-        <Link to='about' className='nav-content-right'>About Us</Link>   
+      <div className="nav-container-right">
+        <form className="form-searchbar" onSubmit={searchSubmit}>
+          <input
+            className="form-searchbar-input"
+            type="text"
+            value={userInput}
+            name="search-bar"
+            aria-label='search-bar'
+            onChange={inputChange}
+            data-testid="searchbar-input"
+          />
+        </form>
+        <Link to="/cart" className="nav-content-right">
+          Cart
+        </Link>
+        <Link to="about" className="nav-content-right">
+          About Us
+        </Link>
       </div>
     </nav>
   );
--- src/Results/Results.js
+++ src/Results/Results.js
@@ -1,13 +1,26 @@
 import "../Results/Results.css";
 
 const Results = ({ result }) => {
+
+  console.log('result in results', result)
+    const {title, image, price, description} = result[0];
+  console.log('getting title', title)
+  // need to map result incase we have multiple
+  // and then render
+  const item = result.map((eachItem) => {
+    return eachItem
+  })
+
+  console.log('mapped item', item)
+  console.log('mapped item.title',item.title)
   return (
     <section className="results-wrapper">
-      {console.log('in result component', result)}
       <div className="results-content">
-        <div className="results-img-container"></div>
+        <div className="results-img-container">
+          <img src={image} alt='item' className='img-sizing'/>
+        </div>
         <div className="results-text-container">
-          <p className="results-content-text">Air jordan 1 centre</p>
+          <p className="results-content-text">{title}</p>
           <p className="results-content-text">Court</p>
           <p className="results-content-price">$135</p>
         </div>
