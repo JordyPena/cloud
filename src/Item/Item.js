4790bea9dd7817eec9b7bdfed21e6efad949d82e
Jordy Pena <jordypenamontes@gmail.com>
2021-06-01 09:46:11 -0500

    item.js created item component to render in results implement test to check render

--- src/Item/Item.js
+++ src/Item/Item.js
@@ -2,21 +2,20 @@
 const Item = ({ item }) => {
   
   return (
-    <section className="results-wrapper">
+    <section className="results-wrapper" data-testid='item-renders'>
     <div className="results-content">
       <div className="results-img-container">
         <img src={item.image} alt='item' className='img-sizing'/>
       </div>
       <div className="results-text-container">
-        <p className="results-content-text">{item.title}</p>
-        <p className="results-content-text">Court</p>
+        <p className="results-content-text" >{item.title}</p>
         <p className="results-content-price">${item.price}</p>
       </div>
     </div>
     <div className="results-content">
       <div className="results-img-container"></div>
       <div className="results-text-container">
-        <p className="results-content-text">Air jordan 1 centre</p>
+        <p className="results-content-text" >Air jordan 1 centre</p>
         <p className="results-content-text">Court</p>
         <p className="results-content-price">$135</p>
       </div>
