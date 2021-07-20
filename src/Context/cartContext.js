ee22bd92dde5cea39f5530fd53f729527ce444f2
Jordy Pena <jordypenamontes@gmail.com>
2021-07-20 11:19:05 -0500

    localStorage ticket --created method to filter items given ids in storage --solved issue were fetch in useEffect ran after the method

--- src/Context/cartContext.js
+++ src/Context/cartContext.js
@@ -1,10 +1,41 @@
-import { createContext, useState, useMemo } from 'react';
+import { createContext, useState, useMemo, useEffect } from 'react';
 
 export const CartContext = createContext({});
 
+const parseLocalStorageCart = (allItems) => {
+  // storeIds is an object
+  
+  const storeIds = JSON.parse(localStorage.getItem('cart'))
+  console.log('ids in cart', storeIds)
+  if (!storeIds) {
+    return []
+  }
+  
+  const foundItems = allItems.filter((item) => {
+    return storeIds.includes(item.id)
+  })
+  console.log('items found', foundItems)
+  return foundItems
+}
+
 export const CartProvider = ({children}) => {
 
+  const [allResults, setAllResults] = useState([]);
   const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+    .then((response) => response.json())
+    .then((data) => {
+      setAllResults(data)
+      setCart(parseLocalStorageCart(data))
+    });
+  }, [])
+
+  useEffect(() => {
+    if (cart.length) 
+    localStorage.setItem('cart', JSON.stringify(cart.map((item) => item.id)))
+  }, [cart])
 
   const addToCart = (item, size) => {
     setCart(prevCart => [...prevCart, { ...item, size} ])
